3f93bbe7d67bafcf7e49cc586f3f7b93b0e39536
jammie-comimts <jay.mbugua.ph@gmail.com>
2024-07-02 21:54:34 +0300

    updated files

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -23,6 +23,51 @@ api = Api(app)
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
+
+class RestaurantsResource(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return jsonify([restaurant.to_dict() for restaurant in restaurants])
+
+class RestaurantResource(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            return jsonify(restaurant.to_dict())
+        return jsonify({"error": "Restaurant not found"}), 404
+
+    def delete(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return '', 204
+        return jsonify({"error": "Restaurant not found"}), 404
+
+class PizzasResource(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return jsonify([pizza.to_dict() for pizza in pizzas])
+
+class RestaurantPizzasResource(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            restaurant_pizza = RestaurantPizza(
+                price=data["price"],
+                pizza_id=data["pizza_id"],
+                restaurant_id=data["restaurant_id"]
+            )
+            db.session.add(restaurant_pizza)
+            db.session.commit()
+            return jsonify(restaurant_pizza.to_dict()), 201
+        except ValueError as e:
+            return jsonify({"errors": [str(e)]}), 400
+
+api.add_resource(RestaurantsResource, "/restaurants")
+api.add_resource(RestaurantResource, "/restaurants/<int:id>")
+api.add_resource(PizzasResource, "/pizzas")
+api.add_resource(RestaurantPizzasResource, "/restaurant_pizzas")
 
 
 if __name__ == "__main__":
